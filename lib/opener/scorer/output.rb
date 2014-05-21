84d6158d623f007e0d04989fa3d1bcc6f4cfcb0d
Giannis Melidis <giannismelidis@olery.com>
2014-05-21 16:12:10 +0200

    remove attr_accessible from models.

--- lib/opener/scorer/output.rb
+++ lib/opener/scorer/output.rb
@@ -3,8 +3,6 @@ require 'active_record'
 module Opener
   class Scorer
     class Output < ActiveRecord::Base
-       attr_accessible :uuid, :text
-
        validates_uniqueness_of :uuid
     end
   end
